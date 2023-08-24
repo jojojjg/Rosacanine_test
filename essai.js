1a9b662a282e11d8aca3e846506503cbe5cf022b
jojojjg <78665756+jojojjg@users.noreply.github.com>
2023-08-24 14:49:02 +0200

    Add files via upload

---
+++ essai.js
@@ -0,0 +1,13 @@
+// On définit la fonction
+
+function afficherDeuxValeurs(valeur1, valeur2) {
+          console.log('Première valeur:' + valeur1);
+          console.log('Deuxième valeur:' + valeur2);
+    }
+    
+    // On exécute la fonction
+    afficherDeuxValeurs(12, 'Bonjour');
+    
+    // On obtient dans la console
+    // > Première valeur:12 
+    // > Deuxième valeur:Bonjour 
